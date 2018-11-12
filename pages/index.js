62f4cd8db8deb628aa82a5d05f629188c3018d58
Ben Orozco <benoror@gmail.com>
2018-11-11 22:07:37 -0600

    Feature: Show rules with react-table

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import 'isomorphic-unfetch'
+import NoSSR from 'react-no-ssr';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 export default class Index extends React.Component {
   static async getInitialProps () {
@@ -15,7 +18,31 @@ export default class Index extends React.Component {
   render () {
     return (
       <div>
-        <p>No. of rules: {this.props.rules.length}</p>
+        <h3>Tax Rules</h3>
+        <ReactTable
+          data={this.props.rules}
+          defaultPageSize={10}
+          className="-striped -highlight"
+          columns={[{
+              Header: 'Country Code',
+              accessor: 'countryCode'
+            },{
+              Header: 'Tax Type',
+              accessor: 'taxType'
+            },{
+              Header: 'BP Type',
+              accessor: 'bpTaxType'
+            },{
+              Header: 'Category',
+              accessor: 'category'
+            },{
+              Header: 'Area',
+              accessor: 'area'
+            },{
+              Header: 'Formula',
+              accessor: 'formula'
+          }]}
+        />
       </div>
     )
   }

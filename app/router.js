933b25c258a696885e74dea16302b65b75ae8f1f
Diego Estrada <destrada@ecaresoft.com>
2020-02-25 16:05:57 -0600

    Feature[simple-auth]: Add routes for authorization

--- app/router.js
+++ app/router.js
@@ -1,12 +1,14 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
-}
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
 
 Router.map(function () {
   this.route('login');
   this.route('home', { path: '/' });
 });
+
+export default Router;

c2478872a4f7703d63babdc446e7b6449d8afc87
Diego Estrada <destrada@ecaresoft.com>
2020-02-25 16:18:24 -0600

    Feature[auth]: Add login form

---
+++ app/pods/login/controller.js
@@ -0,0 +1,11 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  actions: {
+    authenticate() {
+        /*
+         * Authentication Logic
+         */
+    }
+  }
+});

5d411eaceedbc2c11f7794e41d1252e3ed64a62b
Diego Estrada <destrada@ecaresoft.com>
2020-02-25 15:21:01 -0600

    Chore[setup]: Install & configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,8 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true
-    }
+    sourceType: 'module'
   },
   plugins: [
     'ember'
@@ -19,7 +15,70 @@ module.exports = {
     browser: true
   },
   rules: {
-    'ember/no-jquery': 'error'
+    'no-alert': 2,
+    'no-console': 2,
+    'no-debugger': 2,
+    'no-useless-escape': 1,
+    'no-with': 2,
+    'vars-on-top': 1,
+    'wrap-iife': 1,
+    'radix': 2,
+    'eqeqeq': 2,
+    'accessor-pairs': 1,
+    'array-bracket-spacing': 1,
+    'block-scoped-var': 2,
+    'curly': 1,
+    'default-case': 2,
+    'dot-notation': 2,
+    'brace-style': 1,
+    'comma-dangle': 2,
+    'computed-property-spacing': 1,
+    'indent': [1, 2, { 'SwitchCase': 1 }],
+    'block-spacing': 1,
+    'new-cap': 0,
+    'dot-location': ['error', 'property'],
+    'comma-style': ['error', 'last'],
+    'semi': ['error', 'always'],
+
+    'camelcase': [
+      'error', {
+        'properties': 'never'
+      }
+    ],
+
+    'one-var': [
+      'error',
+      'never'
+    ],
+
+    'max-len': [
+      'error', {
+        'code': 90,
+        'ignoreTemplateLiterals': true,
+        'ignoreUrls': true,
+        'ignorePattern': 'import .* from \'.*\';'
+      }
+    ],
+
+    'operator-linebreak': [
+      'error',
+
+      'after', {
+        'overrides': {
+          '+': 'before'
+        }
+      }
+    ],
+
+    'object-shorthand': [
+      'error',
+
+      'always', {
+        'avoidQuotes': true
+      }
+    ],
+
+    'ember/avoid-leaking-state-in-ember-objects': 'off'
   },
   overrides: [
     // node files
@@ -35,7 +94,8 @@ module.exports = {
         'server/**/*.js'
       ],
       parserOptions: {
-        sourceType: 'script'
+        sourceType: 'script',
+        ecmaVersion: 2015
       },
       env: {
         browser: false,

5cd73fd126b1b08c9165c27742cdd51d7945b67f
Diego Estrada <destrada@ecaresoft.com>
2020-03-18 18:48:02 -0600

    Feat[ui]: Implement TaxEngine logo, logout button

--- app/pods/components/user-bar/component.js
+++ app/pods/components/user-bar/component.js
@@ -1,5 +1,10 @@
-import Component from '@ember/component';
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
 
-export default Component.extend({
-  tagName: 'header'
-});
+export default class UserBarComponent extends Component {
+
+  @action
+  logout() {
+    // Logout logic
+  }
+}

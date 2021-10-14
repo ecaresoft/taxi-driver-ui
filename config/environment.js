d1e74f5616939915744c646e006e9ce3039e7c8b
KikeRdz <erodriguez@ecaresoft.com>
2021-10-14 17:17:04 -0500

    Fix: add develop default env

--- config/environment.js
+++ config/environment.js
@@ -32,6 +32,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.baseUrl = 'http://localhost:3000';
   }
 
   if (environment === 'test') {
